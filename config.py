07043790c57caf570c774bc3949ba9cdd03eaf11
Stanislas d'Orsetti <stanislas.dorsetti@gmail.com>
2022-05-03 00:58:17 +0200

    bug fix

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ structure_dict = {
     "output_path": "/content/NLP_project/data/output/",
     "pretrained_vectors_path":"/content/data/pretrained_vectors.pth",
     "path_to_csv": "/content/NLP_project/data/",
-    "path_to_config": "/content/NLP_project/data/"
+    "path_to_config": "/content/NLP_project/data/config.json"
     
 }
